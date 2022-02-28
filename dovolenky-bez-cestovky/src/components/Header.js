bd2c50c35d7f36c27231ddb7e78ce08f9d29625b
Jankois <72013823+Jankois@users.noreply.github.com>
2022-02-28 11:52:32 +0100

    nejake hovno idk

---
+++ dovolenky-bez-cestovky/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../style.css'
+
+function Header() {
+  return (
+    <div>
+        <div className='headerFirstDiv'>
+          <a href='http://localhost:3000/'><h1 className='headerNazov'>Dovolenky bez cestovky</h1></a>
+          <div className='headerSecondDiv'>
+            <a href='http://localhost:3000/'><h2>penis</h2></a>
+            <a href='http://localhost:3000/'><h2>penis</h2></a>
+            <a href='http://localhost:3000/'><h2>penis</h2></a>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
