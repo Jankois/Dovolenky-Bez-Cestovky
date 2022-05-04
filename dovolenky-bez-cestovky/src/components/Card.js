15ede4bff9d648848af817f2c5da66f748b32379
Jankois <72013823+Jankois@users.noreply.github.com>
2022-05-04 19:07:13 +0200

    added popup

--- dovolenky-bez-cestovky/src/components/Card.js
+++ dovolenky-bez-cestovky/src/components/Card.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import Popup from './Popup'
+import {useState} from 'react'
 
 export default function Card(props){
+    const [buttonPopup, setButtonPopup] = useState(false);
     return(
         <div className="item">
             <h2>{props.title}</h2>
@@ -11,7 +14,14 @@ export default function Card(props){
                 <p>{props.descrpition}</p>
                 <h1>{props.price} €</h1>
                 </div>
-                <button id={props.id}>Zisti viac</button>
+                <button id={props.id} onClick={() => setButtonPopup(true)}>Zisti viac</button>
+                <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                    <h2>{props.title}</h2>
+                    <img src={props.imageUrl} alt=""/>
+                    <h3>{props.date}</h3>
+                    <p>{props.descrpition}</p>
+                    <h1>{props.price} €</h1>
+                </Popup>
             </div>
         </div>
     )
