327b24a6619db76e8262bc89d9a875d83f47fe0f
Jankois <72013823+Jankois@users.noreply.github.com>
2022-05-27 19:02:42 +0200

    added login screen

--- backend/src/main/java/com/example/databazos/ArticleControler.java
+++ backend/src/main/java/com/example/databazos/ArticleControler.java
@@ -1,9 +1,7 @@
 package com.example.databazos;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +14,16 @@ public class ArticleControler {
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/articles")
     public Iterable<Articles> getAllArticles(){return articleReposity.findAll();}
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping ("/login")
+    public Boolean loginProcedure(@RequestParam String username, @RequestParam String password){
+        if (username.equals("jozko") && password.equals("ferko")){
+            return true;
+        }
+        return false;
+    }
+
+
 
 }
--- dovolenky-bez-cestovky/src/pages/Admin.js
+++ dovolenky-bez-cestovky/src/pages/Admin.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar'
+import {
+  Grid,
+  TextField,
+  Paper,
+  Button
+} from '@material-ui/core'
+import axios from 'axios'
 
-function admin() {
+function Admin() {
+  
+  const [value , setValue] = useState("");
+  const handleChange = e => {
+  setValue(e.target.value);
+  }
+
+  const [valueDva , setValueDva] = useState("");
+  const handleChangeDva = eDva => {
+  setValueDva(eDva.target.value);
+  }
+
+  const api = axios.create({ baseURL: 'http://localhost:8080/api' })
+
+  function loginProcedure(){
+  api.get('/login', {
+   params: {
+     username: value,
+     password: valueDva
+   }
+  })
+  .then(function(response) {
+   if(response.data == true)
+   {
+     console.log("prihlaseny");
+   }
+   else
+   {
+     console.log("neprihlaseny");
+   }
+  })
+  }
+
   return (
-    <p>Penis</p>
+    <>
+    <Navbar/>
+    <section>
+    <div style={{ padding: 30 }}>
+      <Paper>
+        <Grid
+          container
+          spacing={3}
+          direction={'column'}
+          justify={'center'}
+          alignItems={'center'}
+        >
+          <Grid item xs={12}>
+            <TextField label="Username" value={value} onChange={handleChange}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Password" value={valueDva} onChange={handleChangeDva} type={'password'}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+          </Grid>
+          <Grid item xs={12}>
+            <Button fullWidth onClick={loginProcedure}> Login </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+    </section>
+    </>
   )
 }
 
-export default admin
+export default Admin
